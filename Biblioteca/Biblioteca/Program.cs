14448030ac0be33c550300d69460fc7d3a876b82
JoaoLucas901 <164434877+JoaoLucas901@users.noreply.github.com>
2024-07-25 22:44:29 -0300

    Add files via upload

---
+++ Biblioteca/Biblioteca/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Livro
+    {
+        public string Autor;
+        public string Nome;
+        public int NumberPage;
+
+
+        public void AdicionarLivro()
+        {
+            
+        }
+    }
+}
